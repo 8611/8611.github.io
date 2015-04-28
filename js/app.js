ab307b79e07df0a77ffe5c5e5c02dbf5de152964
8611pl@gmail.com <8611pl@gmail.com>
2015-04-28 03:16:58 +0200

    about section added, stylesheet updated, smooth scroll included

--- js/app.js
+++ js/app.js
@@ -5,3 +5,23 @@ $.backstretch(["img/bg.jpg"], {
 $(function() {
   $('[data-toggle="tooltip"]').tooltip();
 });
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(
+        /^\//,
+        '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +
+        ']');
+      if (target.length) {
+
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
