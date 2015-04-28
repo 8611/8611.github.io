5374fa2c478031027fcdba3c88fc215587745423
8611pl@gmail.com <8611pl@gmail.com>
2015-04-28 12:05:45 +0200

    RadarChart added, about section update

--- js/app.js
+++ js/app.js
@@ -32,3 +32,34 @@ $(window).scroll(function() {
     'transform': 'translate(0px, ' + wScroll / 20 + '%)'
   });
 });
+
+var ctx = document.getElementById("myChart").getContext("2d");
+  
+  var data = {
+    labels: ["PHP", "HTML&CSS", "Javascript", "Ruby on Rails", "C# & .NET", "Java (Android SDK)", "C/C++"],
+    datasets: [
+        {
+            label: "Languages and technologies",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 50, 30, 20, 55, 55, 30]
+        },
+        {
+            label: "Frameworks and libraries",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [60, 60, 50, 30, 40, 40, 20]
+        }
+        
+    ]
+};
+
+var myRadarChart = new Chart(ctx).Radar(data);
